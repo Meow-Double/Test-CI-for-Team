88b6e2d709c848f2ff4aa266d7f460b106155efd
Meow-Double <slava.pugachev.2015@mail.ru>
2024-04-22 21:28:35 +0300

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 
-import viteLogo from '../../../../vite.svg';
-
+// import viteLogo from '../../../../vite.svg';
 import reactLogo from './assets/react.svg';
 
 import './App.css';
@@ -13,7 +12,7 @@ const App = () => {
     <>
       <div>
         <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
+          {/* <img src={viteLogo} className='logo' alt='Vite logo' /> */}
         </a>
         <a href='https://react.dev' target='_blank' rel='noreferrer'>
           <img src={reactLogo} className='logo react' alt='React logo' />
